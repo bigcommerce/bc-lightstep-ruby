ae34c2c464b8fcbd578758f88cea019e574423fb
Shaun <splittingred@gmail.com>
2018-05-23 10:21:29 -0500

    Reduce unnecessary coverage limit

--- spec/simplecov_helper.rb
+++ spec/simplecov_helper.rb
@@ -22,7 +22,7 @@ if File.directory?('./coverage/')
 end
 
 SimpleCov.command_name 'Unit Tests'
-SimpleCov.minimum_coverage 70
+SimpleCov.minimum_coverage 40
 SimpleCov.start do
   add_filter '/bin/'
   add_filter '/coverage/'
