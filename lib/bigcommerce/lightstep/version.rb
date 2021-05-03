7247b2e2bcc383bb85adf391186dffebca2c00b9
Shaun McCormick <splittingred@gmail.com>
2021-05-03 12:31:21 -0500

    Bump to 2.3.2.pre

--- lib/bigcommerce/lightstep/version.rb
+++ lib/bigcommerce/lightstep/version.rb
@@ -8,7 +8,7 @@
 # persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-# Software.
+# Software.have a
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
 # WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -17,6 +17,6 @@
 #
 module Bigcommerce
   module Lightstep
-    VERSION = '2.3.1'
+    VERSION = '2.3.2.pre'
   end
 end
